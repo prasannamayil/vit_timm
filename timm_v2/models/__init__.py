719c22654ae35c4fb878eb7fd769d30707fd6a64
Prasanna Mayilvahanan <solomidcarry@student-net-hg-2016.ethz.ch>
2021-05-20 14:59:16 +0200

    init changes

--- timm_v2/models/__init__.py
+++ timm_v2/models/__init__.py
@@ -1,4 +1,9 @@
-from .version import __version__
-from .models import create_model, list_models, is_model, list_modules, model_entrypoint, \
-    is_scriptable, is_exportable, set_scriptable, set_exportable, has_model_default_key, is_model_default_key, \
-    get_model_default_value, is_model_pretrained
+from .vision_transformer import *
+
+from .factory import create_model, split_model_name, safe_model_name
+from .helpers import load_checkpoint, resume_checkpoint, model_parameters
+from .layers import TestTimePoolHead, apply_test_time_pool
+from .layers import convert_splitbn_model
+from .layers import is_scriptable, is_exportable, set_scriptable, set_exportable, is_no_jit, set_no_jit
+from .registry import register_model, model_entrypoint, list_models, is_model, list_modules, is_model_in_modules,\
+    has_model_default_key, is_model_default_key, get_model_default_value, is_model_pretrained
