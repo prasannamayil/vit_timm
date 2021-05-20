f8d7239442047ea5ed5932d1d07cb9bd519c7b1e
Prasanna Mayilvahanan <solomidcarry@student-net-hg-2016.ethz.ch>
2021-05-20 15:14:45 +0200

    minor changes

--- timm/models/layers/__init__.py
+++ timm/models/layers/__init__.py
@@ -1,11 +1,14 @@
+from .adaptive_avgmax_pool import \
+    adaptive_avgmax_pool2d, select_adaptive_pool2d, AdaptiveAvgMaxPool2d, SelectAdaptivePool2d
 from .config import is_exportable, is_scriptable, is_no_jit, set_exportable, set_scriptable, set_no_jit,\
     set_layer_config
 from .conv2d_same import Conv2dSame, conv2d_same
 from .drop import DropBlock2d, DropPath, drop_block_2d, drop_path
 from .helpers import to_ntuple, to_2tuple, to_3tuple, to_4tuple, make_divisible
 from .linear import Linear
-from .mixed_conv2d import MixedConv2d
 from .mlp import Mlp, GluMlp, GatedMlp
 from .padding import get_padding, get_same_padding, pad_same
 from .patch_embed import PatchEmbed
+from .split_batchnorm import SplitBatchNorm2d, convert_splitbn_model
+from .test_time_pool import TestTimePoolHead, apply_test_time_pool
 from .weight_init import trunc_normal_, variance_scaling_, lecun_normal_
